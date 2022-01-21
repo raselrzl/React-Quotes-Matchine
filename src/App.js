cd5cf924b7ba024274c632d9f12918df8f22b589
Rasel Miah <rasel6041@gmail.com>
2022-01-21 04:38:48 +0100

    Quotes added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Quote from './component/quote';
+
+
+function App() {
+  return (
+    <div>
+        <Quote /> 
+    </div>
+  );
+}
+
+export default App;
